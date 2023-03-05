7824560ef26556cd0332d4bf40e7e44d757f6897
Hayato OKUMOTO <okumoto@twogate.com>
2023-03-05 16:19:07 +0900

    initial commit

---
+++ index.mjs
@@ -0,0 +1,72 @@
+// Webサーバのライブラリ: Express
+import express from 'express';
+import crypto from 'crypto';
+
+// 環境変数の定義を.envファイルから読み込む（開発用途用）
+import dotenv from 'dotenv';
+
+import { LineApi } from './line-api.mjs';
+
+// .envファイル空環境変数を読み込み
+dotenv.config();
+// LINEのチャネルシークレットをCHANNEL_SECRET環境変数から読み込み
+const CHANNEL_SECRET = process.env.CHANNEL_SECRET;
+const CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN;
+
+// expressの初期化
+const app = express();
+app.use(express.json({
+  verify: (req, _res, buf) => {
+    req.rawBody = buf
+  }
+}));
+// TCP/8080ポートでサーバを起動
+app.listen(8080);
+
+
+const lineApi = new LineApi(CHANNEL_ACCESS_TOKEN);
+
+// ルートのエンドポイント定義
+// レスポンスがきちんと返せているかの確認用
+app.get('/', (request, response) => {
+  response.status(200).send('Hello');
+});
+
+// webhookを受け取るエンドポイントを定義
+// POST /webhook
+app.post('/webhook', (request, response, buf) => {
+  // https://developers.line.biz/ja/docs/messaging-api/receiving-messages/
+
+  // 受け取ったwebhookのイベント
+  const body = request.body;
+  // デバッグ用として出力
+  console.log(body);
+
+  // 署名検証（全くの第三者がリクエストを送ってきたときの対策＝なくても動くが本番環境では大事）
+  if (!verifySignature(request.rawBody, request.headers['x-line-signature'], CHANNEL_SECRET)) {
+    response.status(401).send({});
+    return;
+  }
+
+  // 到着したイベントのevents配列から取りだし
+  body.events.forEach(async (event) => {
+    switch (event.type) {
+      case 'message':　// event.typeがmessageのとき応答
+        // 頭に　返信: をつけて、そのまま元のメッセージを返す実装
+        await lineApi.replyMessage(event.replyToken, `返信: ${event.message.text}`);
+        break;
+    }
+  });
+
+  response.status(200).send({});
+});
+
+// webhookの署名検証
+// https://developers.line.biz/ja/reference/messaging-api/#signature-validation
+function verifySignature(body, receivedSignature, channelSecret) {
+  const signature = crypto
+    .createHmac("SHA256", channelSecret)
+    .update(body)
+    .digest("base64");
+  return signature === receivedSignature;
+}
---
+++ line-api.mjs
@@ -0,0 +1,36 @@
+// HTTP APIを実行しやすくするためのライブラリ: Axios
+import axios from 'axios';
+
+// LINE APIのラッパー
+class LineApi {
+  constructor(channelSecret) {
+    this.api = new axios.create({
+      baseURL: 'https://api.line.me/v2',
+      headers: {
+        Authorization: `Bearer ${channelSecret}`,
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  // 応答メッセージAPI
+  async replyMessage(replyToken, message) {
+    const body = {
+      replyToken,
+      messages: [
+        {
+          type: 'text',
+          text: message,
+        }
+      ]
+    };
+
+    return await this.api.post('/bot/message/reply', body);
+  }
+
+  // ここにさらに必要に応じてAPIを呼び出すメソッドを定義しましょう
+}
+
+export {
+  LineApi
+}
