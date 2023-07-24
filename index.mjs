8c85901ed56ee403cf93418556b15ead98fd7ae2
void2610 <izumisan2610@gmail.com>
2023-07-24 11:05:12 +0900

    add for of command

--- index.mjs
+++ index.mjs
@@ -18,11 +18,11 @@ const CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN;
 // expressの初期化
 const app = express();
 app.use(
- express.json({
-  verify: (req, _res, buf) => {
-   req.rawBody = buf;
-  },
- })
+  express.json({
+    verify: (req, _res, buf) => {
+      req.rawBody = buf;
+    },
+  })
 );
 // TCP/8080ポートでサーバを起動
 app.listen(8080);
@@ -35,68 +35,71 @@ let userIds = [];
 // ルートのエンドポイント定義
 // レスポンスがきちんと返せているかの確認用
 app.get("/", (request, response) => {
- response.status(200).send("Hello");
+  response.status(200).send("Hello");
 });
 
 // webhookを受け取るエンドポイントを定義
 // POST /webhook
 app.post("/webhook", (request, response, buf) => {
- // https://developers.line.biz/ja/docs/messaging-api/receiving-messages/
+  // https://developers.line.biz/ja/docs/messaging-api/receiving-messages/
 
- // 受け取ったwebhookのイベント
- const body = request.body;
- // デバッグ用として出力
- console.log(body);
+  // 受け取ったwebhookのイベント
+  const body = request.body;
+  // デバッグ用として出力
+  console.log(body);
 
- // 署名検証（全くの第三者がリクエストを送ってきたときの対策＝なくても動くが本番環境では大事）
- if (!verifySignature(request.rawBody, request.headers["x-line-signature"], CHANNEL_SECRET)) {
-  response.status(401).send({});
-  return;
- }
-
- // 到着したイベントのevents配列から取りだし
- body.events.forEach(async (event) => {
-  switch (event.type) {
-   case "message": // event.typeがmessageのとき応答
-    // 頭に　返信: をつけて、そのまま元のメッセージを返す実装
-    await lineApi.replyMessage(event.replyToken, `返信: ${event.message.text}`);
-
-    // ユーザーIDが配列になかったら格納
-    if (!userIds.includes(event.source.userId)) {
-     userIds.push(event.source.userId);
-     console.log(userIds);
-    }
-    break;
+  // 署名検証（全くの第三者がリクエストを送ってきたときの対策＝なくても動くが本番環境では大事）
+  if (!verifySignature(request.rawBody, request.headers["x-line-signature"], CHANNEL_SECRET)) {
+    response.status(401).send({});
+    return;
   }
- });
 
- response.status(200).send({});
+  // 到着したイベントのevents配列から取りだし
+  body.events.forEach(async (event) => {
+    switch (event.type) {
+      case "message": // event.typeがmessageのとき応答
+        // 頭に　返信: をつけて、そのまま元のメッセージを返す実装
+        await lineApi.replyMessage(event.replyToken, `返信: ${event.message.text}`);
+
+        // ユーザーIDが配列になかったら格納
+        if (!userIds.includes(event.source.userId)) {
+          userIds.push(event.source.userId);
+          console.log(userIds);
+        }
+        break;
+    }
+  });
+
+  response.status(200).send({});
 });
 
 // webhookの署名検証
 // https://developers.line.biz/ja/reference/messaging-api/#signature-validation
 function verifySignature(body, receivedSignature, channelSecret) {
- const signature = crypto.createHmac("SHA256", channelSecret).update(body).digest("base64");
- return signature === receivedSignature;
+  const signature = crypto.createHmac("SHA256", channelSecret).update(body).digest("base64");
+  return signature === receivedSignature;
 }
 
 const rl = readline.createInterface({
- input: process.stdin,
- output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
+//標準入力から関数を呼び出す
 function loopRL() {
- rl.question("input function name: ", async (input) => {
-  const [command, ...args] = input.split(" ");
-  switch (command) {
-   case "push":
-    await lineApi.pushMessage(userIds[0], args.join(" "));
-    break;
-   default:
-    break;
-  }
-  loopRL();
- });
+  rl.question("> ", async (input) => {
+    const [command, ...args] = input.split(" ");
+    switch (command) {
+      case "push":
+        for (let userID of userIds) {
+          await lineApi.pushMessage(userID, args.join(" "));
+        }
+        break;
+      default:
+        break;
+    }
+    loopRL();
+  });
 }
 
 loopRL();
--- line-api.mjs
+++ line-api.mjs
@@ -3,81 +3,46 @@ import axios from "axios";
 
 // LINE APIのラッパー
 class LineApi {
- constructor(channelSecret) {
-  this.api = new axios.create({
-   baseURL: "https://api.line.me/v2",
-   headers: {
-    Authorization: `Bearer ${channelSecret}`,
-    "Content-Type": "application/json",
-   },
-  });
- }
-
- // 応答メッセージAPI
- async replyMessage(replyToken, message) {
-  const body = {
-   replyToken,
-   messages: [
-    {
-     type: "text",
-     text: message,
-    },
-   ],
-  };
-
-  return await this.api.post("/bot/message/reply", body);
- }
-
- // ここにさらに必要に応じてAPIを呼び出すメソッドを定義しましょう
-
- // プッシュメッセージAPI
- async pushMessage(to, message) {
-  const body = {
-   to,
-   messages: [
-    {
-     type: "text",
-     text: message,
-    },
-   ],
-  };
-  return await this.api.post("/bot/message/push", body);
- }
-
- async renderQuickReply() {
-  const body = {
-   type: "text",
-   text: "Select your favorite food category or send me your location!",
-   quickReply: {
-    items: [
-     {
-      type: "action",
-      action: {
-       type: "message",
-       label: "Sushi",
-       text: "Sushi",
+  constructor(channelSecret) {
+    this.api = new axios.create({
+      baseURL: "https://api.line.me/v2",
+      headers: {
+        Authorization: `Bearer ${channelSecret}`,
+        "Content-Type": "application/json",
       },
-     },
-     {
-      type: "action",
-      action: {
-       type: "message",
-       label: "Tempura",
-       text: "Tempura",
-      },
-     },
-     {
-      type: "action",
-      action: {
-       type: "location",
-       label: "Send location",
-      },
-     },
-    ],
-   },
-  };
-  return await this.api.post("/bot/message/push", body);
- }
+    });
+  }
+
+  // 応答メッセージAPI
+  async replyMessage(replyToken, message) {
+    const body = {
+      replyToken,
+      messages: [
+        {
+          type: "text",
+          text: message,
+        },
+      ],
+    };
+
+    return await this.api.post("/bot/message/reply", body);
+  }
+
+  // ここにさらに必要に応じてAPIを呼び出すメソッドを定義しましょう
+
+  // プッシュメッセージAPI
+  async pushMessage(to, message) {
+    const body = {
+      to,
+      messages: [
+        {
+          type: "text",
+          text: message,
+        },
+      ],
+    };
+    return await this.api.post("/bot/message/push", body);
+  }
 }
 
 export { LineApi };
